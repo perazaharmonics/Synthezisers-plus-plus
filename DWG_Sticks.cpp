1d1ff62d261862f8a5a90a02763c9c4c0e7eeabb
Enrique, MEngEE DSP <141517235+perazaharmonics@users.noreply.github.com>
2023-11-04 19:36:34 -0400

    Stick go click
    DWGs

---
+++ DWG_Sticks.cpp
@@ -0,0 +1,243 @@
+/* ------------------------------------------------------------
+name: "untitled", "Josué E. Peraza, B.Sc, M. EngEe"
+Code generated with Faust 2.69.0 (https://faust.grame.fr)
+Compilation options: -lang cpp -ct 1 -es 1 -mcd 16 -single -ftz 0
+------------------------------------------------------------ */
+
+#ifndef  __mydsp_H__
+#define  __mydsp_H__
+
+#ifndef FAUSTFLOAT
+#define FAUSTFLOAT float
+#endif 
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <math.h>
+
+#ifndef FAUSTCLASS 
+#define FAUSTCLASS mydsp
+#endif
+
+#ifdef __APPLE__ 
+#define exp10f __exp10f
+#define exp10 __exp10
+#endif
+
+#if defined(_WIN32)
+#define RESTRICT __restrict
+#else
+#define RESTRICT __restrict__
+#endif
+
+class mydspSIG0 {
+	
+  private:
+	
+	int iVec2[2];
+	int iRec2[2];
+	
+  public:
+	
+	int getNumInputsmydspSIG0() {
+		return 0;
+	}
+	int getNumOutputsmydspSIG0() {
+		return 1;
+	}
+	
+	void instanceInitmydspSIG0(int sample_rate) {
+		for (int l4 = 0; l4 < 2; l4 = l4 + 1) {
+			iVec2[l4] = 0;
+		}
+		for (int l5 = 0; l5 < 2; l5 = l5 + 1) {
+			iRec2[l5] = 0;
+		}
+	}
+	
+	void fillmydspSIG0(int count, float* table) {
+		for (int i1 = 0; i1 < count; i1 = i1 + 1) {
+			iVec2[0] = 1;
+			iRec2[0] = (iVec2[1] + iRec2[1]) % 65536;
+			table[i1] = std::sin(9.58738e-05f * float(iRec2[0]));
+			iVec2[1] = iVec2[0];
+			iRec2[1] = iRec2[0];
+		}
+	}
+
+};
+
+static mydspSIG0* newmydspSIG0() { return (mydspSIG0*)new mydspSIG0(); }
+static void deletemydspSIG0(mydspSIG0* dsp) { delete dsp; }
+
+static float ftbl0mydspSIG0[65536];
+
+class mydsp : public dsp {
+	
+ private:
+	
+	int iVec0[2];
+	FAUSTFLOAT fButton0;
+	float fVec1[2];
+	int iRec0[2];
+	int fSampleRate;
+	float fConst1;
+	float fConst2;
+	float fRec1[2];
+	float fConst3;
+	FAUSTFLOAT fHslider0;
+	float fConst4;
+	float fRec4[2];
+	FAUSTFLOAT fHslider1;
+	FAUSTFLOAT fHslider2;
+	float fRec3[2];
+	
+ public:
+	mydsp() {}
+
+	void metadata(Meta* m) { 
+		m->declare("basics.lib/name", "Faust Basic Element Library");
+		m->declare("basics.lib/tabulateNd", "Copyright (C) 2023 Bart Brouns <bart@magnetophon.nl>");
+		m->declare("basics.lib/version", "1.11.1");
+		m->declare("compile_options", "-lang cpp -ct 1 -es 1 -mcd 16 -single -ftz 0");
+		m->declare("envelopes.lib/adsr:author", "Yann Orlarey and Andrey Bundin");
+		m->declare("envelopes.lib/author", "GRAME");
+		m->declare("envelopes.lib/copyright", "GRAME");
+		m->declare("envelopes.lib/license", "LGPL with exception");
+		m->declare("envelopes.lib/name", "Faust Envelope Library");
+		m->declare("envelopes.lib/version", "1.3.0");
+		m->declare("filename", "untitled.dsp");
+		m->declare("math.lib/author", "GRAME");
+		m->declare("math.lib/copyright", "GRAME");
+		m->declare("math.lib/deprecated", "This library is deprecated and is not maintained anymore. It will be removed in August 2017.");
+		m->declare("math.lib/license", "LGPL with exception");
+		m->declare("math.lib/name", "Math Library");
+		m->declare("math.lib/version", "1.0");
+		m->declare("maths.lib/author", "GRAME");
+		m->declare("maths.lib/copyright", "GRAME");
+		m->declare("maths.lib/license", "LGPL with exception");
+		m->declare("maths.lib/name", "Faust Math Library");
+		m->declare("maths.lib/version", "2.7.0");
+		m->declare("music.lib/author", "GRAME");
+		m->declare("music.lib/copyright", "GRAME");
+		m->declare("music.lib/deprecated", "This library is deprecated and is not maintained anymore. It will be removed in August 2017.");
+		m->declare("music.lib/license", "LGPL with exception");
+		m->declare("music.lib/name", "Music Library");
+		m->declare("music.lib/version", "1.0");
+		m->declare("name", "untitled");
+		m->declare("oscillators.lib/name", "Faust Oscillator Library");
+		m->declare("oscillators.lib/version", "1.4.0");
+		m->declare("platform.lib/name", "Generic Platform Library");
+		m->declare("platform.lib/version", "1.3.0");
+	}
+
+	virtual int getNumInputs() {
+		return 0;
+	}
+	virtual int getNumOutputs() {
+		return 1;
+	}
+	
+	static void classInit(int sample_rate) {
+		mydspSIG0* sig0 = newmydspSIG0();
+		sig0->instanceInitmydspSIG0(sample_rate);
+		sig0->fillmydspSIG0(65536, ftbl0mydspSIG0);
+		deletemydspSIG0(sig0);
+	}
+	
+	virtual void instanceConstants(int sample_rate) {
+		fSampleRate = sample_rate;
+		float fConst0 = std::min<float>(1.92e+05f, std::max<float>(1.0f, float(fSampleRate)));
+		fConst1 = std::max<float>(1.0f, 0.01f * fConst0);
+		fConst2 = 1.0f / fConst1;
+		fConst3 = 1.0f / std::max<float>(1.0f, 0.05f * fConst0);
+		fConst4 = 1.0f / fConst0;
+	}
+	
+	virtual void instanceResetUserInterface() {
+		fButton0 = FAUSTFLOAT(0.0f);
+		fHslider0 = FAUSTFLOAT(2.2e+02f);
+		fHslider1 = FAUSTFLOAT(1.0f);
+		fHslider2 = FAUSTFLOAT(4.4e+02f);
+	}
+	
+	virtual void instanceClear() {
+		for (int l0 = 0; l0 < 2; l0 = l0 + 1) {
+			iVec0[l0] = 0;
+		}
+		for (int l1 = 0; l1 < 2; l1 = l1 + 1) {
+			fVec1[l1] = 0.0f;
+		}
+		for (int l2 = 0; l2 < 2; l2 = l2 + 1) {
+			iRec0[l2] = 0;
+		}
+		for (int l3 = 0; l3 < 2; l3 = l3 + 1) {
+			fRec1[l3] = 0.0f;
+		}
+		for (int l6 = 0; l6 < 2; l6 = l6 + 1) {
+			fRec4[l6] = 0.0f;
+		}
+		for (int l7 = 0; l7 < 2; l7 = l7 + 1) {
+			fRec3[l7] = 0.0f;
+		}
+	}
+	
+	virtual void init(int sample_rate) {
+		classInit(sample_rate);
+		instanceInit(sample_rate);
+	}
+	
+	virtual void instanceInit(int sample_rate) {
+		instanceConstants(sample_rate);
+		instanceResetUserInterface();
+		instanceClear();
+	}
+	
+	virtual mydsp* clone() {
+		return new mydsp();
+	}
+	
+	virtual int getSampleRate() {
+		return fSampleRate;
+	}
+	
+	virtual void buildUserInterface(UI* ui_interface) {
+		ui_interface->openVerticalBox("untitled");
+		ui_interface->addHorizontalSlider("carrierFreq", &fHslider2, FAUSTFLOAT(4.4e+02f), FAUSTFLOAT(2e+01f), FAUSTFLOAT(2e+04f), FAUSTFLOAT(1.0f));
+		ui_interface->addButton("gate", &fButton0);
+		ui_interface->addHorizontalSlider("index", &fHslider1, FAUSTFLOAT(1.0f), FAUSTFLOAT(0.0f), FAUSTFLOAT(1e+01f), FAUSTFLOAT(0.001f));
+		ui_interface->addHorizontalSlider("modulatorFreq", &fHslider0, FAUSTFLOAT(2.2e+02f), FAUSTFLOAT(2e+01f), FAUSTFLOAT(2e+04f), FAUSTFLOAT(1.0f));
+		ui_interface->closeBox();
+	}
+	
+	virtual void compute(int count, FAUSTFLOAT** RESTRICT inputs, FAUSTFLOAT** RESTRICT outputs) {
+		FAUSTFLOAT* output0 = outputs[0];
+		float fSlow0 = float(fButton0);
+		int iSlow1 = fSlow0 == 0.0f;
+		float fSlow2 = fConst4 * float(fHslider0);
+		float fSlow3 = float(fHslider1);
+		float fSlow4 = float(fHslider2);
+		for (int i0 = 0; i0 < count; i0 = i0 + 1) {
+			iVec0[0] = 1;
+			fVec1[0] = fSlow0;
+			iRec0[0] = iSlow1 * (iRec0[1] + 1);
+			fRec1[0] = fSlow0 + fRec1[1] * float(fVec1[1] >= fSlow0);
+			int iTemp0 = 1 - iVec0[1];
+			float fTemp1 = ((iTemp0) ? 0.0f : fSlow2 + fRec4[1]);
+			fRec4[0] = fTemp1 - std::floor(fTemp1);
+			float fTemp2 = ((iTemp0) ? 0.0f : fRec3[1] + fConst4 * (fSlow4 + fSlow3 * ftbl0mydspSIG0[std::max<int>(0, std::min<int>(int(65536.0f * fRec4[0]), 65535))]));
+			fRec3[0] = fTemp2 - std::floor(fTemp2);
+			output0[i0] = FAUSTFLOAT(ftbl0mydspSIG0[std::max<int>(0, std::min<int>(int(65536.0f * fRec3[0]), 65535))] * std::max<float>(0.0f, std::min<float>(fConst2 * fRec1[0], std::max<float>(fConst3 * (fConst1 - fRec1[0]) + 1.0f, 0.0f)) * (1.0f - fConst2 * float(iRec0[0]))));
+			iVec0[1] = iVec0[0];
+			fVec1[1] = fVec1[0];
+			iRec0[1] = iRec0[0];
+			fRec1[1] = fRec1[0];
+			fRec4[1] = fRec4[0];
+			fRec3[1] = fRec3[0];
+		}
+	}
+
+};
+
+#endif
